89580fb919216c7a4154de3f4f97863551278926
flavorhythm <zeyuki@gmail.com>
2016-06-03 14:20:35 -0500

    Added right drawer

--- app/src/main/java/com/echo_usa/echo/MainActivity.java
+++ app/src/main/java/com/echo_usa/echo/MainActivity.java
@@ -1,17 +1,8 @@
 package com.echo_usa.echo;
 
-import android.annotation.TargetApi;
-import android.content.Intent;
-import android.content.res.Configuration;
-import android.content.res.TypedArray;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.ActionBar;
-import android.support.v7.widget.LinearLayoutManager;
-import android.util.TypedValue;
-import android.view.LayoutInflater;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -21,29 +12,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
 
-import com.github.ksoichiro.android.observablescrollview.ObservableRecyclerView;
-import com.github.ksoichiro.android.observablescrollview.ObservableScrollViewCallbacks;
-import com.github.ksoichiro.android.observablescrollview.ScrollState;
-import com.github.ksoichiro.android.observablescrollview.ScrollUtils;
-import com.nineoldandroids.view.ViewHelper;
-
-import adapter.CardAdapter;
-import data.DataAccessObject;
+import fragment.BaseFragment;
+import fragment.FragmentContact;
+import fragment.FragmentGuide;
+import fragment.FragmentDocuments;
+import fragment.FragmentHome;
+import fragment.FragmentLocator;
+import fragment.FragmentMaintenance;
+import fragment.FragmentSettings;
+import fragment.FragmentSpecifications;
+import util.FragName;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener, ObservableScrollViewCallbacks,
-        DrawerLayout.DrawerListener {
-
-    private static final float MAX_TEXT_SCALE_DELTA = 0.3f;
-
-    private View mImageView;
-    private View mOverlayView;
-    private View mRecyclerViewBackground;
-//    private TextView mTitleView;
-    private int mActionBarSize;
-    private int mFlexibleSpaceImageHeight;
+        implements NavigationView.OnNavigationItemSelectedListener, DrawerLayout.DrawerListener,
+        BaseFragment.Callback {
 
     private MenuItem menuItem;
 
@@ -52,110 +35,57 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        mFlexibleSpaceImageHeight = getResources().getDimensionPixelSize(R.dimen.flexible_space_image_height);
-        mActionBarSize = getActionBarSize();
-
-        DataAccessObject dataAccess = new DataAccessObject();
-
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
 
         DrawerLayout drawer = (DrawerLayout)findViewById(R.id.drawer_layout);
         drawer.addDrawerListener(this);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                if(drawerView.equals(findViewById(R.id.nav_menu))) {
+                    super.onDrawerOpened(drawerView);
+                }
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                if(drawerView.equals(findViewById(R.id.nav_menu))) {
+                    super.onDrawerClosed(drawerView);
+                }
+            }
+
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+                if(drawerView.equals(findViewById(R.id.nav_menu))) {
+                    super.onDrawerSlide(drawerView, slideOffset);
+                }
+            }
+        };
+
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        NavigationView navigationView = (NavigationView)findViewById(R.id.nav_menu);
         navigationView.setNavigationItemSelectedListener(this);
 
-        ObservableRecyclerView recycler = (ObservableRecyclerView)findViewById(R.id.recycler);
-        recycler.setScrollViewCallbacks(this);
-
-        final View headerView = LayoutInflater.from(this).inflate(R.layout.header, null);
-        headerView.post(new Runnable() {
-            @Override
-            public void run() {
-                headerView.getLayoutParams().height = mFlexibleSpaceImageHeight;
-            }
-        });
-
-        recycler.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
-        recycler.setAdapter(new CardAdapter(dataAccess.getCards(), headerView));
-
-        mImageView = findViewById(R.id.image);
-        mOverlayView = findViewById(R.id.overlay);
-
-//        mTitleView = (TextView) findViewById(R.id.title);
-//        mTitleView.setText(getTitle());
-//        setTitle(null);
-
-        // mRecyclerViewBackground makes RecyclerView's background except header view.
-        mRecyclerViewBackground = findViewById(R.id.list_background);
-
-        //since you cannot programmatically add a header view to a RecyclerView we added an empty view as the header
-        // in the adapter and then are shifting the views OnCreateView to compensate
-        final float scale = 1 + MAX_TEXT_SCALE_DELTA;
-        mRecyclerViewBackground.post(new Runnable() {
-            @Override
-            public void run() {
-                ViewHelper.setTranslationY(mRecyclerViewBackground, mFlexibleSpaceImageHeight);
-            }
-        });
-        ViewHelper.setTranslationY(mOverlayView, mFlexibleSpaceImageHeight);
-//        mTitleView.post(new Runnable() {
-//            @Override
-//            public void run() {
-//                ViewHelper.setTranslationY(mTitleView, (int) (mFlexibleSpaceImageHeight - mTitleView.getHeight() * scale));
-//                ViewHelper.setPivotX(mTitleView, 0);
-//                ViewHelper.setPivotY(mTitleView, 0);
-//                ViewHelper.setScaleX(mTitleView, scale);
-//                ViewHelper.setScaleY(mTitleView, scale);
-//            }
-//        });
+        setFragment(FragName.HOME);
     }
 
     @Override
     public void onBackPressed() {
+        //TODO: Change so that on back pressed, goes back to home if drawer is closed
+        //use fragment.ishidden or something similar
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
+        } else if (drawer.isDrawerOpen(GravityCompat.END)) {
+            drawer.closeDrawer(GravityCompat.END);
         } else {
             super.onBackPressed();
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.main, menu);
-        return false;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
@@ -168,87 +98,103 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
-    public void onScrollChanged(int scrollY, boolean firstScroll, boolean dragging) {
-        // Translate overlay and image
-        float flexibleRange = mFlexibleSpaceImageHeight - mActionBarSize;
-        int minOverlayTransitionY = mActionBarSize - mOverlayView.getHeight();
-        ViewHelper.setTranslationY(mOverlayView, ScrollUtils.getFloat(-scrollY, minOverlayTransitionY, 0));
-        ViewHelper.setTranslationY(mImageView, ScrollUtils.getFloat(-scrollY / 2, minOverlayTransitionY, 0));
-
-        // Translate list background
-        ViewHelper.setTranslationY(mRecyclerViewBackground, Math.max(0, -scrollY + mFlexibleSpaceImageHeight));
-
-        // Change alpha of overlay
-        ViewHelper.setAlpha(mOverlayView, ScrollUtils.getFloat((float) scrollY / flexibleRange, 0, 1));
-
-        // Scale title text
-//        float scale = 1 + ScrollUtils.getFloat((flexibleRange - scrollY) / flexibleRange, 0, MAX_TEXT_SCALE_DELTA);
-//        setPivotXToTitle();
-//        ViewHelper.setPivotY(mTitleView, 0);
-//        ViewHelper.setScaleX(mTitleView, scale);
-//        ViewHelper.setScaleY(mTitleView, scale);
-
-        // Translate title text
-//        int maxTitleTranslationY = (int) (mFlexibleSpaceImageHeight - mTitleView.getHeight() * scale);
-//        int titleTranslationY = maxTitleTranslationY - scrollY;
-//        ViewHelper.setTranslationY(mTitleView, titleTranslationY);
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 
-    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
-    private void setPivotXToTitle() {
-        Configuration config = getResources().getConfiguration();
-        if (Build.VERSION_CODES.JELLY_BEAN_MR1 <= Build.VERSION.SDK_INT
-                && config.getLayoutDirection() == View.LAYOUT_DIRECTION_RTL) {
-//            ViewHelper.setPivotX(mTitleView, findViewById(android.R.id.content).getWidth());
-        } else {
-//            ViewHelper.setPivotX(mTitleView, 0);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.menu_garage:
+                ((DrawerLayout)findViewById(R.id.drawer_layout)).openDrawer(GravityCompat.END);
+                break;
         }
+
+        return true;
     }
 
     @Override
     public void onDrawerClosed(View drawerView) {
         if(menuItem != null) {
             switch(menuItem.getItemId()) {
+                case R.id.slide_home:
+                    setFragment(FragName.HOME);
+                    break;
                 case R.id.slide_docs:
-                    startIntent("documentation");
+                    setFragment(FragName.DOCS);
                     break;
                 case R.id.slide_maintenace:
-                    startIntent("maintenance");
+                    setFragment(FragName.MAINT);
                     break;
                 case R.id.slide_specs:
-                    startIntent("specifications");
+                    setFragment(FragName.SPECS);
                     break;
                 case R.id.slide_guide:
-                    startIntent("beginners");
+                    setFragment(FragName.GUIDE);
                     break;
                 case R.id.slide_locator:
-                    startIntent("locator");
+                    setFragment(FragName.LOCATOR);
                     break;
                 case R.id.slide_settings:
-                    startIntent("settings");
+                    setFragment(FragName.SETTINGS);
+                    break;
+                case R.id.slide_contact:
+                    setFragment(FragName.CONTACT);
                     break;
                 default:
                     break;
             }
+
+            menuItem = null;
+        }
+    }
+
+    private void setFragment(FragName fragName) {
+        Fragment fragment;
+
+        switch(fragName) {
+            case HOME:
+                fragment = new FragmentHome();
+                break;
+            case DOCS:
+                fragment = new FragmentDocuments();
+                break;
+            case MAINT:
+                fragment = new FragmentMaintenance();
+                break;
+            case SPECS:
+                fragment = new FragmentSpecifications();
+                break;
+            case GUIDE:
+                fragment = new FragmentGuide();
+                break;
+            case LOCATOR:
+                fragment = new FragmentLocator();
+                break;
+            case SETTINGS:
+                fragment = new FragmentSettings();
+                break;
+            case CONTACT:
+                fragment = new FragmentContact();
+                break;
+            default:
+                fragment = null;
+                break;
         }
 
-        menuItem = null;
+        if(fragment != null) {
+            FragmentTransaction fragTrans = getSupportFragmentManager().beginTransaction();
+            //TODO: set animation fragTrans.setCustomAnimations()
+            fragTrans.replace(R.id.main_frag_content, fragment, fragName.toString());
+            fragTrans.commit();
+        }
     }
 
-    private void startIntent(String fragName) {
-        startActivity(new Intent(
-                MainActivity.this, ContentDisplayActivity.class
-        ).putExtra("frag_name", fragName));
-    }
-
-    private int getActionBarSize() {
-        TypedValue typedValue = new TypedValue();
-        int[] textSizeAttr = new int[]{R.attr.actionBarSize};
-        int indexOfAttrTextSize = 0;
-        TypedArray a = obtainStyledAttributes(typedValue.data, textSizeAttr);
-        int actionBarSize = a.getDimensionPixelSize(indexOfAttrTextSize, -1);
-        a.recycle();
-        return actionBarSize;
+    @Override
+    public String getFragName() {
+        return null;
     }
 
     @Override
@@ -259,10 +205,4 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public void onDrawerOpened(View drawerView) {}
-
-    @Override
-    public void onDownMotionEvent() {}
-
-    @Override
-    public void onUpOrCancelMotionEvent(ScrollState scrollState) {}
 }
