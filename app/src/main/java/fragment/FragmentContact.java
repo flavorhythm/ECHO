a1eb426378a3c81c61b7631225791d9ba9eb3bd4
flavorhythm <zeyuki@gmail.com>
2016-06-03 09:26:30 -0500

    Created contact fragment

---
+++ app/src/main/java/fragment/FragmentContact.java
@@ -0,0 +1,25 @@
+package fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.echo_usa.echo.R;
+
+/**
+ * Created by zyuki on 6/2/2016.
+ */
+public class FragmentContact extends BaseFragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        int layoutRes = R.layout.frag_documents;
+        View fragView = inflater.inflate(layoutRes, container, false);
+        ((TextView)fragView.findViewById(R.id.test)).setText(callback.getFragName());
+
+        return fragView;
+    }
+}
