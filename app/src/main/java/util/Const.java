b710674a737540e2ac2deab28e0bc113dbc6f7ea
flavorhythm <zeyuki@gmail.com>
2016-06-03 11:14:48 -0500

    Change Const to FragName enum

--- app/src/main/java/util/Const.java
+++
@@ -1,7 +0,0 @@
-package util;
-
-/**
- * Created by zyuki on 6/2/2016.
- */
-public class Const {
-}
---
+++ app/src/main/java/util/FragName.java
@@ -0,0 +1,22 @@
+package util;
+
+/**
+ * Created by zyuki on 6/2/2016.
+ */
+public enum FragName {
+    HOME("home"),
+    DOCS("documents"),
+    MAINT("maintenance"),
+    SPECS("specifications"),
+    GUIDE("beginners_guide"),
+    LOCATOR("locator"),
+    SETTINGS("settings"),
+    CONTACT("contact");
+
+    String value;
+
+    FragName(String value) {this.value = value;}
+
+    @Override
+    public String toString() {return this.value;}
+}
